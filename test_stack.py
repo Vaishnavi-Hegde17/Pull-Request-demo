e2285e78dcc8527f58b39e4f0120a9640a62d14b
Vaishnavi-Hegde17 <vaishnavi.scs24@bmsce.ac.in>
2025-07-01 18:31:59 +0530

    Initial commit: Stack code, tests, GitHub Actions

---
+++ test_stack.py
@@ -0,0 +1,29 @@
+import unittest
+from Stack import Stack
+
+class TestStack(unittest.TestCase):
+    def test_push_pop(self):
+        s = Stack()
+        s.push(1)
+        s.push(2)
+        self.assertEqual(s.pop(), 2)
+        self.assertEqual(s.pop(), 1)
+
+    def test_peek(self):
+        s = Stack()
+        s.push('x')
+        self.assertEqual(s.peek(), 'x')
+
+    def test_is_empty(self):
+        s = Stack()
+        self.assertTrue(s.is_empty())
+        s.push(10)
+        self.assertFalse(s.is_empty())
+
+    def test_pop_empty(self):
+        s = Stack()
+        with self.assertRaises(IndexError):
+            s.pop()
+
+if __name__ == '__main__':
+    unittest.main()
